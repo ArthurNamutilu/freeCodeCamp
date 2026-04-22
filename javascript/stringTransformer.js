2a9c4bf6fb20b830419b88d994649b0159226a03
Arthur Namutilu <arthurggnamutilu@gmail.com>
2026-04-22 06:51:59 +0300

    transform

--- javascript/stringTransformer.js
+++ javascript/stringTransformer.js
@@ -1,7 +1,6 @@
 const originalString = "I love cats.";
 console.log("Original string:");
 console.log(originalString);
-
 const replacedString = originalString.replace("cats", "dogs");
 console.log("After using the replace() method:");
 console.log(replacedString);
